97760cfac0e0b6def91e9b7206d0c72b024b41e8
ugocuevas <akujobicuevas@gmail.com>
2021-11-30 13:36:11 +0100

    Modified to include two rolls

--- rolldice.py
+++ rolldice.py
@@ -1,32 +1,37 @@
-#In this program, we roll a die and the results of the die determines the next step to take
+#In this program, we roll two fair dice and the results will determine the next step to take
 from random import randint
+from time import sleep
 
-username = input("To test your luck, please enter your name or type exit to leave the game: ")
+username = input("Please enter your name: ")
 username = username.capitalize()
-if username == "Exit":
-    print("Goodbye and see you later")
-    exit()
-else:
-    print(f"Welcome {username}, your die will now be rolled")
 
-#Roll the die    
-die_roll = randint(1,6)
+playOrNot = input("Do you want to roll the dice or exit the game? \n Type Y/N to indicate your choice: ")
+playOrNot = playOrNot.capitalize()
+if playOrNot == "N":
+    print("Goodbye. See you later")
+    exit()
+elif playOrNot == "Y":
+    print(f"Welcome {username}, your dice will now be rolled")   
+else:  
+    print(f"{username}, The game will now exit because you put in an invalid command.\nGoodbye!")
+    exit()
 
 #Determine the outcome of the die throw
-if die_roll == 1:
-    print(f"You rolled {die_roll}. Please drop the die. Better luck next time {username}. Thank you")
-    exit()
-elif die_roll == 6:
-    print(f"You rolled {die_roll}. Move to the next step")
-    secondDieRoll = randint(1, 6)
-    if secondDieRoll == 6:
-        print(f"Second die roll.\n Amazing {username}, you rolled {secondDieRoll}. You are the Champion! ") 
-    else: 
-        print(f"You rolled {secondDieRoll}.\n Not bad {username}, goodbye!")   
-else: 
-    print(f"You rolled {die_roll}. Please roll again")
-    secondDieRoll = randint(1, 6)
-    if secondDieRoll != 6:
-        print(f"You rolled {secondDieRoll}.\n Too bad, {username}, better luck next time. Goodbye!")
+count = 10
+while count > 0:
+#Roll the die    
+    print("Rolling die...")
+    sleep(1)
+    die_roll_one = randint(1,6)
+    die_roll_two = randint(1,6)
+    print(f"You need double sixes to start the game")
+    if die_roll_one == die_roll_two == 6:
+        print(f"Amazing {username}, you rolled double sixes. \nYou can start the game! ")
+        exit()
     else:
-        print(f"Nice throw {username}! You rolled {secondDieRoll}. You are luckier than most players. Goodbye")    
+        print(f"You rolled {die_roll_one} and {die_roll_two}.\nPlease try again")
+    count = count -1
+print(f"Uh oh! {username}, you have exhausted the number of times you can throw.\nYou will now exit the game! ") 
+
+
+
