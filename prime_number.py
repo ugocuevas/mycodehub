8c59e648719626e703e5c50ad00700a481fc9026
ugocuevas <akujobicuevas@gmail.com>
2021-12-05 20:17:44 +0100

    Created by casting into sets and lists

--- prime_number.py
+++ prime_number.py
@@ -3,17 +3,17 @@
 #This demonstrates the first method
 
 #Create the list of numbers
-list_of_numbers = list(range(10, 21))
+list_of_numbers = list(range(10, 21)) 
 print(list_of_numbers)
 
 #Create a list to append the non-prime number
 non_prime_numbers = []
 
 #Iterate through the list and select the non-prime numbers
-for number in list_of_numbers:
-    for i in range(2, number):
+for number in list_of_numbers: 
+    for i in range(2, number): 
         if (number % i) == 0:
             non_prime_numbers.append(number)
             
 non_prime_numbers = list(set(non_prime_numbers))
-print(non_prime_numbers)            
+print(non_prime_numbers)
