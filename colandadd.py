06bdaa52cdc571d15c7f9e0519d2656cdf0ba9a2
ugocuevas <akujobicuevas@gmail.com>
2021-12-13 21:33:57 +0100

    Added a function to the code

--- colandadd.py
+++ colandadd.py
@@ -1,5 +1,15 @@
 #I've only been asked to collect the numbers that you give me and add them up
 firstNumber = input("Please enter the first number: ")
 secondNumber = input("Please enter the second number: ")
-sumOfNumbers = int(firstNumber) + int(secondNumber)
-print(f"You have to believe me: The sum of {firstNumber} and {secondNumber} is {sumOfNumbers}")
+def sumOfNumbers(a, b):
+    a = int(a)
+    b = int(b)
+    c = a - b
+    return c
+print(f"The sum of {firstNumber} and {secondNumber} is {sumOfNumbers(secondNumber, firstNumber)}")
+
+
+# Variables
+#Global: When a variable is declared outside........it can be used anywhere within the script
+#Local: When a variable is declared inside a function, the variable is local and cannot be used outside the variable
+
